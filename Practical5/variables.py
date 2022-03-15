89e7609b453f73b9c10bdc82f8b8d28c52021a28
马红梅 <mahongmei@mahongmeideMacBook-Air.local>
2022-03-15 10:55:10 +0800

    edit variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,11 @@
+a=19245301# number of 2022
+b=4218520# number of 2021
+c=271#number of 2020
+d=b-c #2021-2020
+e=a-b #2022-2021
+print(d>e)
+print(d/c>e/b)#the rate of 2021 > 2022
+
+#the first answer got false,thus d<e
+#the second answer got true, thus the rate of 2021 and 2020
+
