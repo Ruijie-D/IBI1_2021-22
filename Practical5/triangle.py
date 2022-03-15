7ef4a98b213d005d33a6281132fc399dc1b14ac2
马红梅 <mahongmei@mahongmeideMacBook-Air.local>
2022-03-15 13:07:18 +0800

    edit triangle.py

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,7 +1,10 @@
 a=0#a is the sid lehgth of the triangle
 b=0#b is the total dot number of the triangle
+#I need ten triangles
 for a in range(0,10):
+#each time the size length increases, the total number need to add the size length
  a=a+1
  b=b+a
+#print out the triangle number
  print(b)
 
