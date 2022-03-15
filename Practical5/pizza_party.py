f267df3d281f669737b57c35eb6f09f4af2da3db
马红梅 <mahongmei@mahongmeideMacBook-Air.local>
2022-03-15 13:01:36 +0800

    edit pizza_party.py

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -4,10 +4,10 @@ s1a="if I cut the cake "
 s1b=" times, "
 s1c=" pieces of pizza can be gotten" 
 for n in range(0,30): #30 is a random number which promise p>=64 can be reached
- if n>1:
+ if n>=1:
   print(s1a+str(n)+s1b+str(p)+s1c) 
  n=n+1
- p=((n*2)+n+2)/2
+ p=((n**2)+n+2)/2
  #print(p)
  if p>=64:
   break
