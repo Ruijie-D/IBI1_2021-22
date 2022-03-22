da5ac5f63d757cd2831e0c83267f5c0fe7d0a0c0
ooops <mahongmei@ooooops.local>
2022-03-22 12:24:26 +0800

    add robsmark.py

---
+++ Practical6/robsmark.py
@@ -0,0 +1,18 @@
+#input Rob's marks
+M=[45,36,86,57,53,92,65,45]
+print(sorted(M))#print the sorted list
+#make a variable which is the average of the marks in list M
+import numpy as np
+avr=np.mean(M)
+#compare the average with 60
+print(avr>=6)
+#if the average is higher than 60, it will show 'True' otherwise show 'False'
+
+#make a boxplot
+import matplotlib.pyplot as plt
+plt.boxplot(M,sym='D',whis=1.5,notch=False,patch_artist=True)
+
+plt.ylabel('score')
+plt.title("distribution of Rob's mark")
+plt.grid(linestyle="--",alpha=0.7)
+plt.show()
