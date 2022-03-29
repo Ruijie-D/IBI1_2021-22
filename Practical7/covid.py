1ed601f595bf8a084d8415d7236edbb5b0404c6a
ooops <mahongmei@ooooops.local>
2022-03-29 21:40:54 +0800

    add Practical7 directory

---
+++ Practical7/covid.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+import matplotlib.pylot as plt
+import numpy as np
+
+#import the full_data.csv
+covid_data =pd.read_csv("full_data.csv")
+
+#show the first and third columns from rows 10-20
+covid_data.iloc[9:20,0:3:2]
+
+#show "total_cases" for all rows corresponding to Afghanistan
+covid_data.loc[covid_data["location"]=="Afghanistan","total_cases"]#referred to URL:https://blog.csdn.net/weixin_35364187/article/details/113968240
+
+#import the china_new_data.csv
+china_data=pd.read_csv("china_new_data.csv")
+
+#compute the mean number of new cases and new deaths in China
+mean_new_cases=np.mean(china_data.loc[0:,"new_cases"])
+print("mean_new_cases:"+str(mean_new_cases))
+mean_new_deaths=np.mean(china_data.loc[0:,"new_deaths"])
+print("mean_new_deaths:"+str(mean_new_deaths))
+
+#create a boxplot of new cases and new deaths in China
+plt.boxplot(china_data.loc[0:,["new_cases","new_deaths"]],sym="+",whis=0.5,labels=labels)
+labels=['new cases','new deaths']
+plt.title("new cases and new deaths in China in 2020")
+plt.ylabel('number')
+plt.xlabel('class')
+plt.show()
+
+#plot both new cases and new deaths in China over time
+#first create two lists for convenience
+china_dates=china_data.loc[0:,"date"]
+china_new_cases=china_data.loc[0:,"new_cases"]
+plt.plot(china_dates,china_new_cases,'g*')
+plt.xticks(china_dates.iloc[0:len(china_dates):4],rotation=-90)#cited from the practical7 guidance
+plt.ylabel('number')
+plt.xlabel('date')
+plt.title("new cases and new deaths in China over time")
+plt.show()
+
+#answer the question
+spain_data =pd.read_csv("spain_data.csv")
+spain_dates=spain_data.loc[0:,"date"]
+spain_new_cases=spain_data.loc[0:,"new_cases"]
+spain_total_cases=spain_data.loc[0:,"total_cases"]
+plt.plot(spain_dates,spain_new_cases,'b+')
+plt.plot(spain_dates,spain_total_cases,'r+')
+plt.title("new cases and total cases in Spain in 2020 over time")
+plt.plot(2,3,label="new cases")
+plt.plot(2,3* 2,label="total cases")
+plt.xlabel('date')
+plt.ylabel('number')
+plt.legend(loc='best')#legends are referred from URL:https://zhuanlan.zhihu.com/p/70835617
+plt.xticks(china_dates.iloc[0:len(spain_dates):4],rotation=-90)
+plt.show()
+
