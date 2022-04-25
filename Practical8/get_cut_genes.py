9b96f56907fc68b8f65d393db6336e878e8485d3
ooops <mahongmei@ooooops.local>
2022-04-25 20:06:18 +0800

    modify get_cut_genes.py

--- Practical8/get_cut_genes.py
+++ Practical8/get_cut_genes.py
@@ -1,29 +1,35 @@
 import re
 #read the file:
-xfile=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
-#DEBUG:count=0
-orgg=''
-inf=''
-for g in xfile.readlines():
-    if re.match(r'^>',g):#only if the line starts with'>', aiming at faster speeds.
-          if re.search(r'GAATTC',orgg):
-              #DEBUG:count+=1
-              #DEBUG:print(orgg)
-              ##extract information needed:
-              name=re.findall(r'gene:(.+?)\s',inf)
-              leng=len(orgg)
-              #print out matching information:
-              print('>',end='')#refer to URL:https://m.php.cn/article/471647.html
-              print(name,end='')
-              print('      ',end='')
-              print(str(leng))
-              print(orgg)
-          inf=g
-          #DEBUG:print(orgg) 
-          orgg=''
-    else:
-          #avoid sequences spanning two lines:              
-          g=g.rstrip()
-          #keep inputting dna sequence:
-          orgg+=g
+with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')as xfile:
+    #DEBUG:count=0
+    orgg=''
+    inf=''
+    name=''
+    fout=open('cut_genes.fa','w')
+    for g in xfile.readlines():
+        if re.match(r'^>',g):#only if the line starts with'>', aiming at faster speeds.
+              if re.search(r'GAATTC',orgg):
+                  #DEBUG:count+=1
+                  #DEBUG:print(orgg)
+                  #extract information needed:
+                  name=re.findall(r'gene:(.+?)\s',inf)[0]#rrefer to URL: https://www.cnpython.com/qa/42873
+                  leng=len(orgg)
+                  
+                  #print out matching information:
+                  #fout=open('cut_genes.fa','w')
+                  fout.write('>')
+                  fout.write(name)
+                  fout.write('      ')
+                  fout.write(str(leng)+'\n')
+                  fout.write(orgg+'\n')
+                  #fout.close()
+              inf=g
+              #DEBUG:print(orgg) 
+              orgg=''
+        else:
+              #avoid sequences spanning two lines:              
+              g=g.rstrip()
+              #keep inputting dna sequence:
+              orgg+=g
+    fout.close()     
 #DEBUG:print(count)
