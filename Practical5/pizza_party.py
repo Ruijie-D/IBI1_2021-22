3aff28b1886d855c70145897b829646b4a5e9762
ooops <mahongmei@ooooops.local>
2022-04-26 23:19:46 +0800

    modify pizza_party.py

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -3,12 +3,10 @@ p=0 #p is the number of pizza slices
 s1a="if I cut the cake "
 s1b=" times, "
 s1c=" pieces of pizza can be gotten" 
-for n in range(0,30): #30 is a random number which promise p>=64 can be reached
- if n>=1:
-  print(s1a+str(n)+s1b+str(p)+s1c)#it starts to print from the first cut 
+
+while p<64: #p<64 means the slices are not enough and need another cut
  n=n+1
- p=((n**2)+n+2)/2
+ p=((n**2)+n+2)/2 
+ print(s1a+str(n)+s1b+str(p)+s1c)#it starts to print from the first cut 
  #print(p)
- if p>=64:
-  break#when the pizza slices are enough, it can get out of the loop
-print("Whoooo! If I cut "+str(n)+" times, I can get "+str(p)+" slices and it's enough!")
+print("Woooo! If I cut "+str(n)+" times, I can get "+str(p)+" slices and it's enough!")
